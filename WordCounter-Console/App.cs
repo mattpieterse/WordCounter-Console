4443a5c21d69fa2da12bff3086cc8a657643a540
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-03-04 12:14:14 +0200

    Update startup graphic to use plus sign as corner piece

--- WordCounter-Console/App.cs
+++ WordCounter-Console/App.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 namespace WordCounter_Console {
     internal class App {
         public static void Main(string[] args) {
-            Console.WriteLine("|-----------------------------------|");
+            Console.WriteLine("+-----------------------------------+");
             Console.WriteLine("| WordCounter-Console / (pre-0.1.0) |");
-            Console.WriteLine("|-----------------------------------|\n");
+            Console.WriteLine("+-----------------------------------+\n");
         }
     }
 }
